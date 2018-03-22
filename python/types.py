4c771f72872992f148c62ce8cc6570e93b95e8d6
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2018-03-21 23:50:10 -0300

    Add doc

--- python/types.py
+++ python/types.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python
 
-# print(type('hello'))
-print(type([1, 2, 3, 4]))
+# str
+print(type('hello'))
+
+# int
+print(type(1))
+
+# float
+print(type(1.0))
+
+# list
+print(type([1, 2, 3, 4])) 
+
+# tuple (immutable list)
 print(type((1, 2, 3, 4)))
+
+# dict
 print(type({'x': 1, 'y': 1}))
-# print(type(dict(x=1, y=1)))
+print(type(dict(x=1, y=1)))
+print(type({}))
+
+# set
+print(type({1, 2, 3}))
+print(type(set([1, 2, 3])))
