1799c4b6b116f532ab09ca90648b00058e80017b
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2018-04-04 13:57:20 -0300

    Add description to types

--- python/types.py
+++ python/types.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python
 
-# str
+"""str"""
 print(type('hello'))
 
-# int
+"""int"""
 print(type(1))
 
-# float
+"""float"""
 print(type(1.0))
 
-# list
+
+# https://docs.python.org/3.6/tutorial/datastructures.html
+
+"""list
+"""
 print(type([1, 2, 3, 4])) 
 
-# tuple (immutable list)
+"""tuple (immutable list)
+Though tuples may seem similar to lists, they are often used in different
+situations and for different purposes.
+"""
 print(type((1, 2, 3, 4)))
 
-# dict
+
+"""dict
+Dictionaries are sometimes found in other languages as “associative memories” or “associative arrays”.
+It's best to think of a dictionary as an unordered set of key: value pairs.
+
+strings and numbers can always be keys
+the keys are unique
+"""
 print(type({'x': 1, 'y': 1}))
 print(type(dict(x=1, y=1)))
 print(type({}))
 
-# set
+"""set
+A set is an unordered collection with no duplicate elements.
+"""
 print(type({1, 2, 3}))
 print(type(set([1, 2, 3])))
