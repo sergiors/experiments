532105bdb07c229459f9c69acb8756a0586d6690
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2018-04-04 10:11:13 -0300

    Add more details about exception

--- python/exception.py
+++ python/exception.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+# https://docs.python.org/2/tutorial/errors.html#defining-clean-up-actions
+
 try:
     raise NameError
 except NameError:
@@ -8,3 +10,8 @@ except KeyboardInterrupt:
     print('KeyboardInterrupt')
 finally:
     print('finally')
+
+try:
+    pass
+finally:
+    print('A finally clause is always executed before leaving the try statement, whether an exception has occurred or not')
